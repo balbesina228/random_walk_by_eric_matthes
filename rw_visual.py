d9bed80bf1957b698582224f7786a3c23c8b7272
balbesina228 <109335756+balbesina228@users.noreply.github.com>
2022-08-25 11:45:33 +0700

    Add files via upload

--- rw_visual.py
+++ rw_visual.py
@@ -1,10 +1,17 @@
 import matplotlib.pyplot as plt
 from random_walk import RandomWalk
 
-rw = RandomWalk()
-rw.fill_walk()
+while True:
+    rw = RandomWalk()
+    rw.fill_walk()
 
-plt.style.use('ggplot')
-fig, ax = plt.subplots()
-ax.scatter(rw.x_values, rw.y_values, s=15)
-plt.show()
+    plt.style.use('ggplot')
+    fig, ax = plt.subplots()
+    point_numbers = range(rw.num_points)
+    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues,
+               edgecolors='none', s=15)
+    plt.show()
+
+    keep_going = input('Continue? (y/n): ')
+    if keep_going == 'n':
+        break
